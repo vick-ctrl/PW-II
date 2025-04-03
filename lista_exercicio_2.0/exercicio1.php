955cb8904b94526987e16987054cbe9a4cea3c3a
vick-ctrl <victoria.oliv2015@gmail.com>
2025-04-03 17:19:57 -0300

    lista php

---
+++ lista_exercicio_2.0/exercicio1.php
@@ -0,0 +1,29 @@
+<?php
+  $alunos = [
+    'aluno1' => [
+        'nome' => 'victoria',
+        'rm' => '24102006',
+        'foto' => ,
+        'telefone' => 
+    ],
+    'aluno2' => [
+        'nome' => 'victoria',
+        'rm' => '24102006',
+        'foto' => ,
+        'telefone' => 
+    ],
+    'aluno3' => [
+        'nome' => 'victoria',
+        'rm' => '24102006',
+        'foto' => ,
+        'telefone' => 
+    ],
+    'aluno4' => [
+        'nome' => 'victoria',
+        'rm' => '24102006',
+        'foto' => ,
+        'telefone' => 
+    ]
+
+];
+?>
