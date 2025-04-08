f767a05e88d15a711a79a0000a660faa4d283afd
vick-ctrl <victoria.oliv2015@gmail.com>
2025-04-07 21:50:37 -0300

    Update exercicio2.php
    links das imagens dos meu dogs

--- lista_exercicio_2.0/exercicio2.php
+++ lista_exercicio_2.0/exercicio2.php
@@ -7,7 +7,7 @@ $zoologico = [
         'cor' => 'preto e branco',
         'peso' => '11kg',
         'idade' => '14 anos',
-        'foto' => '',
+        'foto' => 'https://mail.google.com/mail/u/0?ui=2&ik=1eaecc4278&attid=0.1&permmsgid=msg-a:r-1561014170361556689&th=19612de447e68e4c&view=fimg&fur=ip&permmsgid=msg-a:r-1561014170361556689&sz=s0-l75-ft&attbid=ANGjdJ8_AXYuVoFgIpcC1va-VzJPlJochszJxhJPJGOEDMViCETpqFftm3AH5p7WYkbztwPYQlUdQKf8vswkhaL-_ZZemCy0lvMI835ld7qmg83mx_VOFmwHUKC1V8U&disp=emb&realattid=0B7D7E7E-A921-47A4-961E-151A2A22EF8B&zw',
     ],
     'animal2' => [
         'nome' => 'Baby',
@@ -16,7 +16,7 @@ $zoologico = [
         'cor' => 'preto',
         'peso' => '5kg',
         'idade' => '12 anos',
-        'foto' => '',
+        'foto' => 'https://mail.google.com/mail/u/0?ui=2&ik=1eaecc4278&attid=0.1&permmsgid=msg-a:r3913967665449439200&th=19612de0ca87270d&view=fimg&fur=ip&permmsgid=msg-a:r3913967665449439200&sz=s0-l75-ft&attbid=ANGjdJ_VkX0oV18g79orP0tSzBU-nb-R8yOhdYpCqkH51LpDXR9ewqNCkTuAgezjOwXhAUHDmtYrjIvjDTNAmGrYQd3uhtCcExyCV9jNZ1rgck49K6VzE2lpKvJ6-5Q&disp=emb&realattid=49B95F3D-1629-4AB2-B97A-072574F63CE1&zw',
     ],
     'animal3' => [
         'nome' => 'mimosa',
