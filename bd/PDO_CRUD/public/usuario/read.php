e81d9198f2d95d3dafbc12b3a011507897610467
vick-ctrl <victoria.oliv2015@gmail.com>
2025-05-08 18:26:44 -0300

    banco

---
+++ bd/PDO_CRUD/public/usuario/read.php
@@ -0,0 +1,22 @@
+<?php
+   include '../../config/connection.php';
+   
+   $stmt = $pdo -> query('SELECT * FROM usuario');
+   $usuarios = $stmt -> fetchAll();
+
+//    echo "<pre>";
+//    echo var_dump($usuarios);
+ 
+?>
+
+<?php foreach ($usuarios as $indice => $user) { ?>
+   <p><strong>Nome:</strong><?php echo $user["username"];?></p>
+   <a href="">remover</a>
+   <a href="">editar</a>
+   <hr>
+<?php } ?>
+
+
+
+  
+ 
